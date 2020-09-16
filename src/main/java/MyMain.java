e2a9480c42ebcff673d7d9e64c4b3053f40ca6a4
fwa7son <21fwatson@athenian.org>
2020-09-16 02:25:10 +0000

    added functionality to program. It passed all tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,30 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(String binary) {
+        int binary2 = Integer.parseInt(binary);
+        int returned_decimal = 0;
+        int power = 0;
+        while (true){
+            if (binary2 == 0){
+                break;
+            }
+            else {
+                int tempvar = binary2 % 10;
+                returned_decimal += tempvar*Math.pow(2,power);
+                binary2 = binary2 / 10;
+                power++;
+            }
+        }
+        return returned_decimal;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a binary number: ");
+        String binary = scan.next();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(binary));
+        scan.close();
     }
 }
